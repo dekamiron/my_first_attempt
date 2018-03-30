b38eb60e055a6953b6b5624d87a37e4aecdc25c2
Gena Tranov <36458530+dekamiron@users.noreply.github.com>
2018-03-31 00:04:51 +0300

    Update task00_socer.py

--- homeWork_socer/task00_socer.py
+++ homeWork_socer/task00_socer.py
@@ -1,15 +1,15 @@
 class Team:
     def __init__(self, id_team, name_team, players):
         self.id_team = id_team
-        self.name_team = name_team
+        self.name = name
         self.players = players
 
 
 class Player:
-    def __init__(self, id_player, name_player, name_team):
+    def __init__(self, id_player, name, team):
         self.id_player = id_player
-        self.name = name_player
-        self.name_team = name_team
+        self.name = name
+        self.team = team
 
 
 class Match:
